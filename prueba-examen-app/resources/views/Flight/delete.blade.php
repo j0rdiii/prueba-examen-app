20643c328eb7f8770e2e489093c577b1e4df5de4
Jordi Lumbreras <jordilumbrerasaragon@a.cifpperedesongall.org>
2025-02-19 20:52:19 +0100

    Deleted

--- prueba-examen-app/resources/views/Flight/delete.blade.php
+++
@@ -1,5 +0,0 @@
-<form action="{{ route('Flights.destroy', $flight) }}" method="post">
-    @method('DELETE')
-    @csrf
-    <button type="submit">Delete</button>
-</form>
--- prueba-examen-app/resources/views/Flight/update.blade.php
+++
@@ -1,3 +0,0 @@
-<td>
-    <a href="{{ route('Fligths.edit', $flight) }}">Update</a>
-</td>
